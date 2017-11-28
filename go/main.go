30a8e2a60045d28234f691f0f739944737b525fd
Perez, Philipp <Philipp.Perez@opitz-consulting.com>
2017-11-28 19:09:42 +0100

    add go app

---
+++ go/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+var counter = 0
+
+func main() {
+	router := mux.NewRouter()
+	router.HandleFunc("/", HomeHandler)
+	http.Handle("/", router)
+	log.Println("listerning on :8080")
+	http.ListenAndServe(":8080", router)
+}
+
+func HomeHandler(writer http.ResponseWriter, request *http.Request) {
+
+	headerMap := request.Header
+	acc := headerMap["Accept"]
+	enc := headerMap["Accept-Encoding"]
+	lang := headerMap["Accept-Language"]
+	cache := headerMap["Cache-Control"]
+	con := headerMap["Connection"]
+	host := headerMap["Host"]
+	pragma := headerMap["Pragma"]
+	userAgent := headerMap["User-Agent"]
+	upgradeSSL := headerMap["Upgrade-Insecure-Requests"]
+	log.Println("received request", acc, enc, lang, cache, con, host, pragma, upgradeSSL, userAgent)
+
+	counter++
+	log.Println("visitor count:", counter)
+
+	writer.Write([]byte("hello, world\n"))
+	writer.Write([]byte(fmt.Sprintf("visitor count: %d", counter)))
+}
